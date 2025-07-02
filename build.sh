d44f3422e32f97160361f83024d309964e76c70c
DannyZhou06 <68139996+DannyZhou06@users.noreply.github.com>
2025-07-01 19:22:27 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ pip install -r requirements.txt
 # Run database migrations
 flask db upgrade
 
+flask db create admin
+
 # (Optional) Create admin user if it doesn't exist.
 # This is a simple way to ensure the admin is always available.
 # Note: In a real production app, you might handle this differently.
